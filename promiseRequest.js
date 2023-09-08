ad3994d86dad775b6a68fb9961c6fe0fa0e336bd
Nicholas Ward <102345071+NCF94@users.noreply.github.com>
2023-09-08 10:43:02 +1000

    feat: add API data image to screen

---
+++ promiseRequest.js
@@ -0,0 +1,31 @@
+const API_BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
+
+
+function getRandomPokemon(){
+    // let result = fetch("https://pokeapi.co/api/v2/pokemon/" + 25).then()
+    // let result = fetch("https://pokeapi.co/api/v2/pokemon/25").then()
+
+    let ID = 25;
+    let result = fetch(API_BASE_URL + ID).then((response) => {
+        console.log("Response is: " + JSON.stringify(response, null, 4));
+
+        return response.json();
+
+    })
+    .then(data => {
+        console.log("Data is: " + JSON.stringify(data, null, 4));
+    
+        console.log("Data retrieved for: " +data.name);
+        console.log("Image is: " + data.sprites.other["official-artwork"].front_default);
+
+        let pkmImage = document.getElementById("pokemonImage");
+        pkmImage.src = data.sprites.other["official-artwork"].front_default;
+        
+        return data;
+    }).catch(error => {
+        console.log(error);
+    });
+
+    // console.log("Result of API request is:" + result);
+
+}
